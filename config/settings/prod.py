2317f422d9ef395092822aaebb79d1916af201b4
kyle <chang4293@gmail.com>
2023-01-31 15:05:34 +0900

    서버 재설정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['43.201.88.236', 'codenageune.kr']
+ALLOWED_HOSTS = ['34.239.38.242', 'codenageune.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = True
